d1578aa77ed310429389959b9237809f5fe7b20d
Vivek <atrivivek001@gmail.com>
2025-02-18 16:10:38 +0530

    pay

--- lib/actions/transaction.action.ts
+++ lib/actions/transaction.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 import Stripe from "stripe";
 import { handleError } from '../utils';
 import { connectToDatabase } from '../database/mongoose';
@@ -8,34 +8,40 @@ import Transaction from '../database/models/transaction.model';
 import { updateCredits } from './user.actions';
 
 export async function checkoutCredits(transaction: CheckoutTransactionParams) {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  try {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-  const amount = Number(transaction.amount) * 100;
+    const amount = Number(transaction.amount) * 100;
 
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          unit_amount: amount,
-          product_data: {
-            name: transaction.plan,
-          }
-        },
-        quantity: 1
-      }
-    ],
-    metadata: {
-      plan: transaction.plan,
-      credits: transaction.credits,
-      buyerId: transaction.buyerId,
-    },
-    mode: 'payment',
-    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
-    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-  })
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            unit_amount: amount,
+            product_data: {
+              name: transaction.plan,
+            }
+          },
+          quantity: 1
+        }
+      ],
+      metadata: {
+        plan: transaction.plan,
+        credits: transaction.credits,
+        buyerId: transaction.buyerId,
+      },
+      mode: 'payment',
+      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+      cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+    });
 
-  redirect(session.url!)
+    // Return the session URL so the frontend can handle the redirection
+    return session.url;
+
+  } catch (error) {
+    handleError(error);
+  }
 }
 
 export async function createTransaction(transaction: CreateTransactionParams) {
@@ -45,12 +51,12 @@ export async function createTransaction(transaction: CreateTransactionParams) {
     // Create a new transaction with a buyerId
     const newTransaction = await Transaction.create({
       ...transaction, buyer: transaction.buyerId
-    })
+    });
 
     await updateCredits(transaction.buyerId, transaction.credits);
 
     return JSON.parse(JSON.stringify(newTransaction));
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
